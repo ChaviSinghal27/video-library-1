c7e83f2c77ee82eff63f402df44cd103b3c51293
ChaviSinghal27 <108614142+ChaviSinghal27@users.noreply.github.com>
2023-09-20 14:50:21 +0530

    add: home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,11 +1,16 @@
 import { LeftBar } from "../../components/Leftbar";
 import { Navbar } from "../../components/Navbar";
+import { VideoDisplay } from "../../components/VideoDisplay";
+import { data } from "../../utilities/data";
 
 export function Home() {
   return (
     <div>
       <Navbar />
-      <LeftBar />
+      <div>
+        <LeftBar />
+        <VideoDisplay videoData={data.videoData} />
+      </div>
     </div>
   );
 }
