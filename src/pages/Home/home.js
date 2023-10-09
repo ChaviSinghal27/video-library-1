fa4bf1507bc8c1868f569b98323117301e2595b6
ChaviSinghal27 <108614142+ChaviSinghal27@users.noreply.github.com>
2023-10-09 14:10:41 +0530

    update: home

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,15 +1,13 @@
-import { LeftBar } from "../../components/Leftbar";
-import { Navbar } from "../../components/Navbar";
 import { VideoDisplay } from "../../components/VideoDisplay";
 import { data } from "../../utilities/data";
 
 export function Home() {
   return (
     <div>
-      <Navbar />
-      <div>
-        <LeftBar />
-        <VideoDisplay videoData={data.videoData} />
+      <div className="main">
+        <div className="videos">
+          <VideoDisplay videoData={data.videoData} />
+        </div>
       </div>
     </div>
   );
